28b41743fe434b9b0af0291641f7de14124a02d3
sarabrie <sarabr0278@gmail.com>
2026-05-26 23:15:39 +0300

    Add files via upload

---
+++ HomeWork_4/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Threading;
+
+namespace HomeWork_4;
+
+class Program
+{
+    static void Main()
+    {
+        TimeUnit seconds = new TimeUnit("Seconds", 60);
+        TimeUnit minutes = new TimeUnit("Minutes", 60);
+        TimeUnit hours = new TimeUnit("Hours", 24);
+
+        hours.SetValue(10);
+        minutes.SetValue(59);
+        seconds.SetValue(57);
+
+        seconds.LimitReached += (sender, e) => minutes.Tic();
+        minutes.LimitReached += (sender, e) => hours.Tic();
+
+        AlarmClock alarm = new AlarmClock(11, 0, minutes, hours);
+
+        Display clockDisplay = new Display(seconds, minutes, hours, alarm);
+
+        while (true)
+        {
+            seconds.Tic();
+            Thread.Sleep(1000);
+        }
+    }
+}
---
+++ HomeWork_4/TimeReport.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace HomeWork_4;
+
+public class TimeReport : EventArgs
+{
+    public int CurrentValue { get; }
+
+    public TimeReport(int currentValue)
+    {
+        CurrentValue = currentValue;
+    }
+}
